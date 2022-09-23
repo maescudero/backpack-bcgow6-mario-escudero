6e543564d6945d65da7504029478acaf193daeb9
maescudero <mario.escudero@mercadolibre.com>
2022-09-23 13:05:17 -0300

    goBases added fixing

---
+++ goBases1/ejercicio3.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+	//seria incorrecto 1nombre string por que no se puede inciar con un numero --- var 1nombre string
+	// int edad incorrecto por que es al reves, int es tipo de dato ---- var apellido string
+	// estaria correcto ----- var int edad
+	// 1apellido:=0 incorrecto por -----1apellido := 6
+	//corecto -----var licencia_de_conducir = true
+	//incorrecto por que no puede tener espacions una variable ----var estatura de la persona int
+	//corecto cantidadDeHijos := 2
+}
---
+++ goBases1/ejercicio4.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+	//var apellido string = "Gomez"
+	// var edad int = 35  -- sacarle las comillas
+	// isTrue := false --- sacarle las comillas y cambiar la nombre de variable para no confundir
+	// var sueldo float64 = 45857.90 cambiar el tipo de dato a float
+	// var nombre string = "Julián"
+
+}
