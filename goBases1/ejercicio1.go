7639ff80dbc14cbcfb472d48e3cfdd3d1df9334b
maescudero <mario.escudero@mercadolibre.com>
2022-09-23 13:01:49 -0300

    goBases added

---
+++ goBases1/ejercicio1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Mario Escudero"
+	direccion := "Faldeo Sur Manzana L Casa Nº 6"
+
+	fmt.Println("mi nombre es: " + nombre + " y vivo en: " + direccion)
+}
---
+++ goBases1/ejercicio2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	humedad := "21%"
+	temperatura := 21.1
+	presion := "1013hPa"
+
+	fmt.Println("humedad: " + humedad)
+	fmt.Println("temperatura: ", temperatura)
+	fmt.Println("presion: ", presion)
+
+}
