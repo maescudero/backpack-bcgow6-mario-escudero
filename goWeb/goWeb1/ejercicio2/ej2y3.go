25a19ec8591248c594c1e993d3d75010d9c95bb4
maescudero <mario.escudero@mercadolibre.com>
2022-10-04 12:34:03 -0300

    go web2 TM

--- goWeb/goWeb1/ejercicio2/ej2y3.go
+++ goWeb/goWeb1/ejercicio2/ej2y3.go
@@ -12,39 +12,44 @@ import (
 	//"fmt"
 	//	"os"
 
-	"fmt"
+	"encoding/json"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/maescudero/backpack-bcgow6-mario-escudero/goWeb/goWeb1/ejercicio1"
 )
 
-/*func get() string {
+type Product struct {
+	ID        string
+	Nombre    string
+	Color     string
+	Precio    float64
+	Stock     int64
+	Codigo    string
+	Publicado bool
+	fecha     string
+}
 
-	data, _ := os.ReadFile("/goWeb/goWeb1/ejercicio1/productos.json")
+func GetProducts() (products []Product, err error) {
+	data, err0 := os.ReadFile("./productos.json")
+	if err0 != nil {
+		err = err0
+	}
 
-	fmt.Println(string(data))
+	err1 := json.Unmarshal(data, &products)
+	if err1 != nil {
+		err = err1
+	}
 
-	return string(data)
-} */
+	return
+}
 
 func main() {
 
 	router := gin.Default()
 
-	data, err := os.ReadFile("./productos.json")
-
-	if err != nil {
-		fmt.Println("omar algo anda mal")
-	}
-	x := string(data)
-
-	router.GET("/get", func(ctx *gin.Context) {
-
-		ctx.JSON(200, gin.H{
-			"data": x,
-		})
-	})
+	//creamos la ruta para devolver los productos
+	router.GET("/products", GetAll)
 
 	router.GET("/saludo", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
@@ -53,5 +58,46 @@ func main() {
 	})
 
 	router.Run()
+
+}
+
+func GetAll(c *gin.Context) {
+
+	var products []Product
+	var err error
+
+	products, err = GetProducts()
+
+	if err != nil {
+		c.JSON(500, err)
+		return
+	}
+
+	var productsResponse []Product
+
+	//logica de filtrado
+	productName := c.Query("name")
+	productColour := c.Query("colour")
+	productPrice := c.Query("price")
+	productStock := c.Query("stock")
+	productCreatedAt := c.Query("createdAt")
+
+	if productName == "" && productColour == "" && productPrice == "" && productStock == "" && productCreatedAt == "" {
+		c.JSON(200, products)
+	} else {
+
+		priceKey, _ := strconv.ParseFloat(productPrice, 64)
+		stockKey, _ := strconv.ParseInt(productStock, 0, 0)
+
+		for _, product := range products {
+
+			if (productName != "" && product.Nombre == productName) || (productColour != "" && product.Color == productColour) || (productPrice != "" && product.Precio == priceKey) || (productStock != "" && int(product.Stock) == int(stockKey)) || (productCreatedAt != "" && product.fecha == productCreatedAt) {
+				productsResponse = append(productsResponse, product)
+			}
+		}
+
+		c.JSON(200, productsResponse)
+
+	}
 
 }
---
+++ goWeb/goWeb2/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+/*
+Ejercicio 1 - Crear Entidad
+Se debe implementar la funcionalidad para crear la entidad. pasa eso se deben seguir los
+siguientes pasos:
+1. Crea un endpoint mediante POST el cual reciba la entidad.
+2. Se debe tener un array de la entidad en memoria (a nivel global), en el cual se
+deberán ir guardando todas las peticiones que se vayan realizando.
+3. Al momento de realizar la petición se debe generar el ID. Para generar el ID se debe
+buscar el ID del último registro generado, incrementarlo en 1 y asignarlo a nuestro
+nuevo registro (sin tener una variable de último ID a nivel global).
+
+Ejercicio 2 - Validación de campos
+Se debe implementar las validaciones de los campos al momento de enviar la petición, para
+eso se deben seguir los siguientes pasos:
+1. Se debe validar todos los campos enviados en la petición, todos los campos son
+requeridos
+2. En caso que algún campo no esté completo se debe retornar un código de error 400
+con el mensaje “el campo %s es requerido”.
+(En %s debe ir el nombre del campo que no está completo).
+
+Ejercicio 3 - Validar Token
+Para agregar seguridad a la aplicación se debe enviar la petición con un token, para eso se
+deben seguir los siguientes pasos::
+
+1. Al momento de enviar la petición se debe validar que un token sea enviado
+2. Se debe validar ese token en nuestro código (el token puede estar hardcodeado).
+3. En caso que el token enviado no sea correcto debemos retornar un error 401 y un
+mensaje que “no tiene permisos para realizar la petición solicitada”.
+
+
+*/
+
+type Product struct {
+	Id     int
+	Nombre string  `json:"nombre" binding:"required"`
+	Color  string  `json:"color" binding:"required"`
+	Precio float64 `json:"precio" binding:"required"`
+	Stock  int64   `json:"stock" binding:"required"`
+}
+
+var productos []Product
+var Id int
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/registro", registo())
+
+	router.Run()
+
+}
+
+func registo() gin.HandlerFunc {
+
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
+
+		if token != "messi" || token == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "no tiene permisos para realizar la petición solicitada"})
+			return
+		}
+
+		var req Product
+		err := ctx.ShouldBindJSON(&req)
+		if err != nil {
+			ctx.JSON(404, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		comprobar := req.checkFilters(ctx)
+
+		if comprobar {
+			Id++
+			req.Id = Id
+			productos = append(productos, req)
+			ctx.JSON(200, productos)
+		}
+
+	}
+
+}
+
+func (p Product) checkFilters(ctx *gin.Context) bool {
+	if p.Nombre == "" {
+		respuesta := fmt.Sprintf("el campo %s es requerido”", p.Nombre)
+		ctx.JSON(http.StatusBadRequest, respuesta)
+		return false
+	}
+	if p.Color == "" {
+		respuesta := fmt.Sprintf("el campo %s es requerido”", p.Color)
+		ctx.JSON(http.StatusBadRequest, respuesta)
+		return false
+	}
+	if p.Precio < 0 {
+		respuesta := fmt.Sprintf("el campo %f es requerido”", p.Precio)
+		ctx.JSON(http.StatusBadRequest, respuesta)
+		return false
+	}
+	if p.Stock < 0 {
+		respuesta := fmt.Sprintf("el campo %v es requerido”", p.Stock)
+		ctx.JSON(http.StatusBadRequest, respuesta)
+		return false
+	}
+
+	return true
+
+}
