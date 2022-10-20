4cffcb241aa3f6ec31cba3e842a0ddcf32cbbe1d
maescudero <mario.escudero@mercadolibre.com>
2022-10-20 15:14:42 -0300

    Exercise 1  testing TM2 added

---
+++ testing/testing2TM/TardeTT/cmd/server/handler/product.go
@@ -0,0 +1,171 @@
+package handler
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/maescudero/backpack-bcgow6-mario-escudero/testing/testing2TM/TardeTT/internal/products"
+	"github.com/maescudero/backpack-bcgow6-mario-escudero/testing/testing2TM/TardeTT/pkg/web"
+)
+
+type request struct {
+	Name  string  `json:"nombre"`
+	Type  string  `json:"tipo"`
+	Count int     `json:"cantidad"`
+	Price float64 `json:"precio"`
+}
+
+type Product struct {
+	service products.Service
+}
+
+func NewProduct(p products.Service) *Product {
+	return &Product{
+		service: p,
+	}
+}
+
+// ListProducts godoc
+// @Summary List products
+// @Tags Products
+// @Description get products
+// @Produce  json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /products [get]
+func (c *Product) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(401, web.NewResponse(401, nil, "token invalido"))
+			return
+		}
+
+		p, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(401, web.NewResponse(500, nil, err.Error()))
+			return
+		}
+
+		if len(p) == 0 {
+			ctx.JSON(401, web.NewResponse(404, nil, "no hay productos"))
+			return
+		}
+
+		ctx.JSON(200, web.NewResponse(200, p, ""))
+	}
+}
+
+func (c *Product) Store() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.Request.Header.Get("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(401, web.NewResponse(401, nil, "token invalido"))
+			return
+		}
+
+		var req request
+		if err := ctx.Bind(&req); err != nil {
+			ctx.JSON(400, web.NewResponse(401, nil, err.Error()))
+			return
+		}
+
+		if req.Name == "" {
+			ctx.JSON(400, web.NewResponse(401, nil, "el nombre del prod es requerido"))
+			return
+		}
+		if req.Type == "" {
+			ctx.JSON(400, web.NewResponse(401, nil, "el tipo del prod es requerido"))
+			return
+		}
+		if req.Count == 0 {
+			ctx.JSON(400, web.NewResponse(401, nil, "la cantidad del prod es requerido"))
+			return
+		}
+		if req.Price == 0 {
+			ctx.JSON(400, web.NewResponse(401, nil, "el precio del prod es requerido"))
+			return
+		}
+
+		p, err := c.service.Store(req.Name, req.Type, req.Count, req.Price)
+		if err != nil {
+			ctx.JSON(400, web.NewResponse(401, nil, err.Error()))
+			return
+		}
+		ctx.JSON(200, web.NewResponse(200, p, ""))
+	}
+}
+
+func (c *Product) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("token")
+		if token != os.Getenv("TOKEN") {
+			ctx.JSON(401, web.NewResponse(401, nil, "token invalido"))
+			return
+		}
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(401, web.NewResponse(400, nil, "ID invalido"))
+			//ctx.JSON(400, gin.H{"error": "invalid ID"})
+			return
+		}
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			ctx.JSON(401, web.NewResponse(401, nil, err.Error()))
+			return
+		}
+		if req.Name == "" {
+			ctx.JSON(400, web.NewResponse(401, nil, "el nombre del prod es requerido"))
+			return
+		}
+		if req.Type == "" {
+			ctx.JSON(400, web.NewResponse(401, nil, "el tipo del prod es requerido"))
+			return
+		}
+		if req.Count == 0 {
+			ctx.JSON(400, web.NewResponse(401, nil, "la cantidad del prod es requerido"))
+			return
+		}
+		if req.Price == 0 {
+			ctx.JSON(400, web.NewResponse(401, nil, "el precio del prod es requerido"))
+			return
+		}
+		p, err := c.service.Update(int(id), req.Name, req.Type, req.Count, req.Price)
+		if err != nil {
+			ctx.JSON(400, web.NewResponse(401, nil, "algo"))
+			return
+		}
+		ctx.JSON(200, web.NewResponse(200, p, ""))
+	}
+}
+
+func (p *Product) UpdateNameAndType() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("token")
+		if token != os.Getenv("TOKEN") {
+			c.JSON(401, web.NewResponse(401, nil, "token invalido"))
+			return
+		}
+		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+		if err != nil {
+			c.JSON(401, web.NewResponse(400, nil, "ID invalido"))
+			//ctx.JSON(400, gin.H{"error": "invalid ID"})
+			return
+		}
+		var req request
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		p, err := p.service.UpdateNameAndType(int(id), req.Name, req.Type)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, p)
+	}
+}
---
+++ testing/testing2TM/TardeTT/cmd/server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/maescudero/backpack-bcgow6-mario-escudero/testing/testing2TM/TardeTT/cmd/server/handler"
+	"github.com/maescudero/backpack-bcgow6-mario-escudero/testing/testing2TM/TardeTT/docs"
+	"github.com/maescudero/backpack-bcgow6-mario-escudero/testing/testing2TM/TardeTT/internal/products"
+	"github.com/maescudero/backpack-bcgow6-mario-escudero/testing/testing2TM/TardeTT/pkg/store"
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+)
+
+// @title           Bootcamp Go Wave 6 - API
+// @version         1.0
+// @termsOfService  https://developers.mercadolibre.com.ar/es_ar/terminos-y-condiciones
+
+// @contact.name   API Support Digital House
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+
+func main() {
+
+	_ = godotenv.Load()
+
+	db := store.New(store.FileType, "./products.json")
+	repo := products.NewRepository(db)
+	service := products.NewService(repo)
+	p := handler.NewProduct(service)
+
+	r := gin.Default()
+
+	docs.SwaggerInfo.Host = os.Getenv("HOST")
+	r.GET("docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	pr := r.Group("/products")
+	pr.POST("/", p.Store())
+	pr.GET("/", p.GetAll())
+	pr.PUT("/:id", p.Update())
+	pr.PATCH("/:id", p.UpdateNameAndType())
+
+	r.Run()
+}
